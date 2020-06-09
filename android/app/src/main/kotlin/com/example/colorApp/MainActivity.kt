ad8b47aa26de976dd772701ff5159d8bbed7e802
Aryan G <29174147+Trolled00Gamer@users.noreply.github.com>
2020-06-10 00:01:51 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/colorApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.colorApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,206 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey[50],
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+ValueNotifier<int> red = ValueNotifier<int>(0);
+ValueNotifier<int> green = ValueNotifier<int>(0);
+ValueNotifier<int> blue = ValueNotifier<int>(0);
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+  // ValueNotifier<int> red = ValueNotifier<int>(0);
+  // ValueNotifier<int> green = ValueNotifier<int>(0);
+  // ValueNotifier<int> blue = ValueNotifier<int>(0);
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int i = 0;
+  int abc = 0xff212121;
+  // ValueNotifier<int> red = ValueNotifier<int>(0);
+  // ValueNotifier<int> green = ValueNotifier<int>(0);
+  // ValueNotifier<int> blue = ValueNotifier<int>(0);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenheight = MediaQuery.of(context).size.height;
+    int somenumber() {
+      i++;
+      if (i > 7) i = 7;
+      return i;
+    }
+
+    return Scaffold(
+      backgroundColor: Color(abc),
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Container(
+            padding: new EdgeInsets.fromLTRB(0, screenheight * 0.05, 0, 0),
+            child: Text(
+              "Color",
+              style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: Theme.of(context).textTheme.headline3.fontSize),
+            ),
+          ),
+          Padding(
+            padding: new EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                color: Color.fromRGBO(red.value, green.value, blue.value, 1),
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 0.66 * screenheight,
+                      width: MediaQuery.of(context).size.width,
+                    ),
+                    ColorSliders(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          //Expanded(
+          //child:
+          // GridView.count(
+          //     crossAxisCount: 4,
+          //     children: List.generate(i, (index) {
+          //       return Container(
+          //         child: Text(
+          //           'lorem ipsum dolor',
+          //           style: new TextStyle(color: Color(abc)),
+          //         ),
+          //       );
+          //     })),
+          // //     ),
+          Container(
+            width: MediaQuery.of(context).size.width - 50,
+            padding: new EdgeInsets.fromLTRB(0, 20, 0, 20),
+            child: GridView.count(
+                shrinkWrap: true, //made the grid list work inside column
+                crossAxisCount: 4,
+                children: List.generate(i, (index) {
+                  return ColorBox();
+                })),
+          ),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            somenumber();
+          });
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ColorBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+      color: Color(0xffffffffF),
+      child: Container(
+        height: 50,
+        width: 50,
+      ),
+    );
+  }
+}
+
+class ColorSliders extends StatefulWidget {
+  // ColorSliders({
+  //   Key key,
+  //   @required this.red,
+  //   @required this.green,
+  //   @required this.blue,
+  // }) : super(key: key);
+  @override
+  _ColorSlidersState createState() => _ColorSlidersState();
+  // ValueNotifier<int> red = ValueNotifier<int>(0);
+  // ValueNotifier<int> green = ValueNotifier<int>(0);
+  // ValueNotifier<int> blue = ValueNotifier<int>(0);
+}
+
+class _ColorSlidersState extends State<ColorSliders> {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      right: 0,
+      left: 0,
+      child: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.blue,
+            height: 50,
+            width: 50,
+          ),
+          Slider(
+            label: red.value.toString(),
+            value: red.value.toDouble(),
+            min: 0,
+            max: 255,
+            divisions: 255,
+            onChanged: (double _newvalue) {
+              setState(() {
+                print(_newvalue.toInt());
+                red.value = _newvalue.toInt();
+              });
+            },
+          ),
+          Slider(
+            label: green.value.toString(),
+            value: green.value.toDouble(),
+            min: 0,
+            max: 255,
+            divisions: 255,
+            onChanged: (double _newvalue) {
+              setState(() {
+                print(_newvalue.toInt());
+                green.value = _newvalue.toInt();
+              });
+            },
+          ),
+          Slider(
+            label: blue.value.toString(),
+            value: blue.value.toDouble(),
+            min: 0,
+            max: 255,
+            divisions: 255,
+            onChanged: (double _newvalue) {
+              setState(() {
+                print(_newvalue.toInt());
+                blue.value = _newvalue.toInt();
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
