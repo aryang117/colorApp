a5e18e95ecad8c2ebd049a1a41ac0f02e72594bb
Trolled00Gamer <aryu100@gmail.com>
2021-04-18 21:12:17 +0530

    fixed/re-added bottom-nav-bar, solid and Gradient both accessible from it

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -2,11 +2,7 @@ import 'package:colorApp/Screens/gradient.dart';
 import 'package:colorApp/Screens/solidColor.dart';
 import 'package:flutter/material.dart';
 
-// import 'package:persistent_bottom_nav_bar/models/persisten-bottom-nav-item.widget.dart';
-// import 'package:persistent_bottom_nav_bar/models/persisten-bottom-nav-item.widget.dart';
-// import 'package:persistent_bottom_nav_bar/models/persistent-bottom-nav-bar-styles.widget.dart';
-// import 'package:persistent_bottom_nav_bar/models/persistent-nav-bar-scaffold.widget.dart';
-// import 'package:persistent_bottom_nav_bar/persistent-tab-view.widget.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -17,71 +13,66 @@ class _HomeState extends State<Home> {
   int i = 0;
   int abc = 0xffffffff;
 
-  // PersistentTabController _controller;
+  PersistentTabController _controller;
 
   @override
   void initState() {
     super.initState();
-    // _controller = PersistentTabController(initialIndex: 0);
+    _controller = PersistentTabController(initialIndex: 0);
+  }
+
+  List<Widget> _buildScreens() {
+    return [
+      SolidColor(),
+      GradientColor(),
+      SolidColor(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.check_box_outline_blank),
+        title: ("Solid"),
+        activeColor: Colors.white,
+        inactiveColor: Colors.grey,
+        // isTranslucent: false,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.gradient),
+        title: ("Gradient"),
+        activeColor: Colors.white,
+        inactiveColor: Colors.grey,
+        //   isTranslucent: false,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.settings),
+        title: ("Settings"),
+        activeColor: Colors.white,
+        inactiveColor: Colors.grey,
+        //  isTranslucent: false,
+      ),
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    return GradientColor();
+    return PersistentTabView(
+        controller: _controller,
+        screens: _buildScreens(),
+        items:
+            _navBarsItems(), // Redundant here but defined to demonstrate for other than custom style
+        confineInSafeArea: true,
+        //elevation: true,
+        backgroundColor: Color(0xff282828),
+        handleAndroidBackButtonPress: true,
+        onItemSelected: (int) {
+          setState(
+              () {}); // This is required to update the nav bar if Android back button is pressed
+        },
+        itemCount: 4,
+        navBarStyle:
+            NavBarStyle.style13 // Choose the nav bar style with this property
+        );
   }
-
-  // List<Widget> _buildScreens() {
-  //   return [
-  //     SolidColor(),
-  //     GradientColor(),
-  //     SolidColor(),
-  //   ];
-  // }
-
-  // List<PersistentBottomNavBarItem> _navBarsItems() {
-  //   return [
-  //     PersistentBottomNavBarItem(
-  //       icon: Icon(Icons.check_box_outline_blank),
-  //       title: ("Solid"),
-  //       activeColor: Colors.white,
-  //       inactiveColor: Colors.grey,
-  //      // isTranslucent: false,
-  //     ),
-  //     PersistentBottomNavBarItem(
-  //       icon: Icon(Icons.gradient),
-  //       title: ("Gradient"),
-  //       activeColor: Colors.white,
-  //       inactiveColor: Colors.grey,
-  //    //   isTranslucent: false,
-  //     ),
-  //     PersistentBottomNavBarItem(
-  //       icon: Icon(Icons.settings),
-  //       title: ("Settings"),
-  //       activeColor: Colors.white,
-  //       inactiveColor: Colors.grey,
-  //     //  isTranslucent: false,
-  //     ),
-  //   ];
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return PersistentTabView(
-  //       controller: _controller,
-  //       screens: _buildScreens(),
-  //       items:
-  //           _navBarsItems(), // Redundant here but defined to demonstrate for other than custom style
-  //       confineInSafeArea: true,
-  //       showElevation: true,
-  //       backgroundColor: Color(0xff282828),
-  //       handleAndroidBackButtonPress: true,
-  //       onItemSelected: (int) {
-  //         setState(
-  //             () {}); // This is required to update the nav bar if Android back button is pressed
-  //       },
-  //       itemCount: 4,
-  //       navBarStyle:
-  //           NavBarStyle.style9 // Choose the nav bar style with this property
-  //       );
-  // }
 }
