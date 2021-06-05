5055c5b3b0e937ed7aebc2789e1ee574498d6c78
Trolled00Gamer <aryu100@gmail.com>
2021-06-05 20:35:16 +0530

    Made the copy to clipboard button a separate stf widget

--- lib/Widgets/solidColorTextField.dart
+++ lib/Widgets/solidColorTextField.dart
@@ -24,42 +24,68 @@ class _SolidColorTextFieldState extends State<SolidColorTextField> {
       child: TextFormField(
         style: TextStyle(color: Colors.white),
         decoration: InputDecoration(
-          border:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-          prefixText: "#  " +
-              widget.red.toRadixString(16) +
-              "" +
-              widget.green.toRadixString(16) +
-              "" +
-              widget.blue.toRadixString(16),
-          prefixStyle: TextStyle(color: Colors.white, fontSize: 20),
-          suffixIcon: IconButton(
-            icon: Icon(Icons.copy_rounded),
-            onPressed: () {
-              final snackBar = SnackBar(
-                content: Text('Copied Color ' + ' to Clipboard!'),
-                action: SnackBarAction(
-                  label: 'DISMISS',
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).removeCurrentSnackBar();
-                  },
-                ),
-              );
-              Clipboard.setData(new ClipboardData(
-                  text: 'Red: ' +
-                      widget.red.toRadixString(16) +
-                      ' Green: ' +
-                      widget.green.toRadixString(16) +
-                      ' Blue: ' +
-                      widget.blue.toRadixString(16)));
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-            },
-          ),
-        ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            prefixText: "#  " +
+                widget.red.toRadixString(16) +
+                "" +
+                widget.green.toRadixString(16) +
+                "" +
+                widget.blue.toRadixString(16),
+            prefixStyle: TextStyle(color: Colors.white, fontSize: 20),
+            suffixIcon: CopyToClipBoardButton(
+              red: red.value,
+              green: green.value,
+              blue: blue.value,
+            )),
         keyboardType: TextInputType.number,
         maxLength: 6,
         readOnly: true,
       ),
+    );
+  }
+}
+
+//This icon lets the use copy the color that they just created using the sliders
+class CopyToClipBoardButton extends StatefulWidget {
+  const CopyToClipBoardButton({Key key, this.red, this.green, this.blue})
+      : super(key: key);
+
+  final int red;
+  final int green;
+  final int blue;
+
+  @override
+  _CopyToClipBoardButtonState createState() => _CopyToClipBoardButtonState();
+}
+
+class _CopyToClipBoardButtonState extends State<CopyToClipBoardButton> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        Icons.copy_rounded,
+        color: Colors.white,
+      ),
+      onPressed: () {
+        final snackBar = SnackBar(
+          content: Text('Copied Color ' + ' to Clipboard!'),
+          action: SnackBarAction(
+            label: 'DISMISS',
+            onPressed: () {
+              ScaffoldMessenger.of(context).removeCurrentSnackBar();
+            },
+          ),
+        );
+        Clipboard.setData(new ClipboardData(
+            text: 'Red: ' +
+                widget.red.toRadixString(16) +
+                ' Green: ' +
+                widget.green.toRadixString(16) +
+                ' Blue: ' +
+                widget.blue.toRadixString(16)));
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      },
     );
   }
 }
